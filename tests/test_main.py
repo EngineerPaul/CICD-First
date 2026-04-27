37863d32fa1ad5c216143d7c5f6254c8eae3f38a
EngineerPaul <kpashka@mail.ru>
2026-04-27 21:07:55 +0400

    test changed

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 def test_root_hello_world(client) -> None:
     r = client.get("/")
     assert r.status_code == 200
-    assert r.json() == {"message": "Hello, world!"}
+    assert r.json() == {"message": "Hello world!"}
 
